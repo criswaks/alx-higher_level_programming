b61229a3bdefcdedf9b94e9ac08c88f99949d908
criswaks <criswaks@gmail.com>
2022-06-08 20:07:37 -0700

    Commit python addition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-    from add_0 import add
+from add_0 import add
 
+
+def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+
+if __name__ == "__main__":
+    main()
